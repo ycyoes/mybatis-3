7158770d9acfd438027254bcda14633d91391d05
ycyoes <ycyoes@163.com>
2020-03-08 19:10:38 +0800

    调用者

--- src/main/java/org/apache/ibatis/reflection/invoker/Invoker.java
+++ src/main/java/org/apache/ibatis/reflection/invoker/Invoker.java
@@ -18,10 +18,24 @@ package org.apache.ibatis.reflection.invoker;
 import java.lang.reflect.InvocationTargetException;
 
 /**
+ * 调用者接口
  * @author Clinton Begin
  */
 public interface Invoker {
+
+  /**
+   * 执行一次调用。而具体调用什么方法，由子类来实现。
+   *
+   * @param target 目标
+   * @param args 参数
+   * @return 结果
+   * @throws IllegalAccessException
+   * @throws InvocationTargetException
+   */
   Object invoke(Object target, Object[] args) throws IllegalAccessException, InvocationTargetException;
 
+  /**
+   * @return 类
+   */
   Class<?> getType();
 }
